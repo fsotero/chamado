54add582e71f679b8530b727454678acb21ddfea
Felipe Sotero Galdino <felipe.galdino@laura-br.com>
2022-11-09 17:11:59 -0300

    Guarapuava completo

--- Unimed_Guarapuava_completo.py
+++ Unimed_Guarapuava_completo.py
@@ -293,8 +293,8 @@ def coleta_envia_payload_graphql(ti):
         for payload in payloads:
             data = transforma_payload(payload)
             print(data)
-            print('json: ',json.dumps(data, ensure_ascii=False))
-            r = requests.post("https://saude.fastmedic.com.br/api.integracao/api/IntegracaoConsulta/SalvarConsultaIntegracao", data=json.dumps(data, ensure_ascii=False), headers={"Content-type" : "application/json", "Authorization" : fastmedic_token})
+            print('json: ',json.dumps(data, ensure_ascii=False)) #, ensure_ascii=False
+            r = requests.post("https://saude.fastmedic.com.br/api.integracao/api/IntegracaoConsulta/SalvarConsultaIntegracao", data=json.dumps(data), headers={"Content-type" : "application/json", "Authorization" : fastmedic_token})
             print(r.status_code)
             print(r.json())
         paginas = paginas + 1
